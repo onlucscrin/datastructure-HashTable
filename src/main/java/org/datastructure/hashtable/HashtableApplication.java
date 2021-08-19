2108b7eb9ffe23c740290257d259b538f7fde056
onlucscrin <vinutha.m.reddy@gmail.com>
2021-08-18 23:37:40 -0400

    Initial version

---
+++ src/main/java/org/datastructure/hashtable/HashtableApplication.java
@@ -0,0 +1,13 @@
+package org.datastructure.hashtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashtableApplication.class, args);
+	}
+
+}
